7cce2393cde2357735e20c81a4bdcb541ea2d2dd
taketaketakeyuki <taketaketakeyuki@gmail.com>
2020-03-01 14:46:46 +0900

    first

--- project/settings.py
+++ project/settings.py
@@ -128,3 +128,6 @@ LOGOUT_REDIRECT_URL='/'
 
 # django-crispy-forms 設定
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
